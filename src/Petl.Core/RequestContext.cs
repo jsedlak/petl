87db7ad5e89abdad456da95cb92aac3dbf006de8
John Sedlak <john.sedlak@outlook.com>
2024-01-10 07:57:43 -0500

    Adds user id and property bag to request context

--- src/Petl.Core/RequestContext.cs
+++ src/Petl.Core/RequestContext.cs
@@ -5,4 +5,8 @@ public class RequestContext
     public Guid CorrelationId { get; set; } = Guid.NewGuid();
 
     public DateTimeOffset RequestedAt { get; set; } = DateTimeOffset.UtcNow;
+
+    public string? UserIdentifier { get; set; }
+
+    public Dictionary<string, string> Properties { get; set; } = new Dictionary<string, string>();
 }
---
+++ src/Petl.Core/RequestResult.cs
@@ -0,0 +1,21 @@
+﻿namespace Petl;
+
+public class RequestResult : IRequestResult
+{
+    public RequestResult()
+    {
+
+    }
+
+    public RequestResult(IEnumerable<IResponse> responses)
+    {
+        Responses = responses;
+    }
+
+    public RequestResult(params IResponse[] responses)
+    {
+        Responses = responses;
+    }
+
+    public IEnumerable<IResponse> Responses { get; } = Enumerable.Empty<IResponse>();
+}
